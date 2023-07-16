94292c641c92f2d98011797739691e89e1978624
“Jay <jaykumarnitrr@icloud.com>
2023-07-16 16:29:15 +0530

    Some codes to rectify

--- db/model.js
+++ db/model.js
@@ -2,54 +2,54 @@ const { DataTypes } = require("sequelize");
 const { db } = require("./connections");
 
 const Course = db.define("course", {
-  id: {
-    type: DataTypes.STRING(2),
-    primaryKey: true
-  },
-  name: DataTypes.STRING(40)
+    id: {
+        type: DataTypes.STRING(2),
+        primaryKey: true
+    },
+    name: DataTypes.STRING(40)
 });
 
 const Teacher = db.define("teacher", {
-  name: {
-    type: DataTypes.STRING(35),
-    allowNull: false
-  }
+    name: {
+        type: DataTypes.STRING(35),
+        allowNull: false
+    }
 });
 
 const Center = db.define("center", {
-  id: {
-    type: DataTypes.STRING(2),
-    primaryKey: true
-  },
-  name: {
-    type: DataTypes.STRING(20),
-    allowNull: false
-  },
-  city: {
-    type: DataTypes.STRING(20),
-    allowNull: false
-  }
+    id: {
+        type: DataTypes.STRING(2),
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    city: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    }
 });
 
 const Season = db.define("season", {
-  id: {
-    type: DataTypes.STRING(1),
-    primaryKey: true
-  },
-  name: {
-    type: DataTypes.STRING(10),
-    allowNull: false
-  }
+    id: {
+        type: DataTypes.STRING(1),
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    }
 });
 
 const Batch = db.define("batch", {
-  code: {
-    type: DataTypes.STRING(8), //WDPP18S1
-    primaryKey: true
-  },
-  year: DataTypes.INTEGER(4),
-  start: DataTypes.DATE,
-  end: DataTypes.DATE
+    code: {
+        type: DataTypes.STRING(8), //WDPP18S1
+        primaryKey: true
+    },
+    year: DataTypes.INTEGER(4),
+    start: DataTypes.DATE,
+    end: DataTypes.DATE
 });
 
 //Associations
@@ -64,10 +64,10 @@ Season.hasMany(Batch);
 db.sync();
 
 module.exports = {
-  db,
-  Course,
-  Teacher,
-  Center,
-  Season,
-  Batch
+    db,
+    Course,
+    Teacher,
+    Center,
+    Season,
+    Batch
 };
